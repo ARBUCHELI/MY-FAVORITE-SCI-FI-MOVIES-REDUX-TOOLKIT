e700ad9e0a508ba12cfe7ddc1216762a1f2c4599
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-11-04 20:35:35 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const allRecipesData = [
+    { id: 0, name: 'Abyss', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/abyss.jpg'},
+    { id: 1, name: 'Annihilation', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/annihilation.jpg'},
+    { id: 2, name: 'Arrival', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/arrival.jpg'},
+    { id: 3, name: 'Avatar', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/avatar.jpg'},
+    { id: 4, name: 'District 9', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/district9.jpg'},
+    { id: 5, name: 'Duna', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/duna.jpg'},
+    { id: 6, name: 'E.T.', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/et.jpg'},
+    { id: 7, name: 'Gatacca', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/gatacca.jpg'},
+    { id: 8, name: 'High-Life', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/high-life.jpg'},
+    { id: 9, name: 'Interstellar', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/interstellar.jpg'},
+    { id: 10, name: 'Minority Report', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/minority-report.jpg'},
+    { id: 11, name: 'Moon', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/moon.jpg'},
+    { id: 12, name: 'Primer', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCI-FI-MOVIES-REDUX-TOOLKIT/master/img/primer.jpg'},
+    { id: 13, name: 'Snowpiercer', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/snowpiercer.jpg'},
+    { id: 14, name: 'Terminator 2', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/terminator-2.jpg'},
+    { id: 15, name: 'The Man Who Fell', img: 'https://raw.githubusercontent.com/ARBUCHELI/MY-FAVORITE-SCIE-FI-MOVIES-REDUX-TOOLKIT/master/img/the-man-who-fell-to-earth.jpg'},
+  ];
+  
+  export default allRecipesData;
