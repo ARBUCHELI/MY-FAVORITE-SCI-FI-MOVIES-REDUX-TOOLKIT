151eefb5213a350a5649d55f6e32b7b1e354edd7
Andres R. Bucheli <radio_seattle@hotmail.com>
2022-11-07 17:30:09 +0300

    First Commit

--- src/app/App.js
+++ src/app/App.js
@@ -10,14 +10,17 @@ export function App() {
       <section>
         <SearchTerm />
       </section>
-      <section>
-        <h2 style={{color: "#e4eff0"}}>My Favorite Sci-Fi Movies</h2>
+      <section>   
+        <h2 style={{color: "white", backgroundColor: "#034f84", width: "25%", padding: "0.5em", border: "1px transparent solid", borderRadius: "5px"}}>Books already read</h2>
         <FavoriteRecipes />
       </section>
       <hr />
       <section>
-        <h2 style={{color: "#e4eff0"}}>All Movies</h2>
+        <h2 style={{color: "white", backgroundColor: "#810000", width: "25%", padding: "0.5em", border: "1px transparent solid", borderRadius: "5px"}}>30 scientific divulgation books that you must read</h2>
         <AllRecipes />
+      </section>
+      <section>
+        <footer className="app-footer">Bucheli Web Development © 2022</footer>
       </section>
     </main>
   )
